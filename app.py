ef020982eed49655b4933935e2c502e4a337e08b
matheushfsi <matheushfsilva2007@gmail.com>
2026-06-01 08:06:06 -0300

    refactor: organizaçãp e limpeza do código do app.py

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template, request, redirect, session
 import mysql.connector
 
 app = Flask(__name__)
-
 app.secret_key = "fsc"
 
 def conectar():
-
     return mysql.connector.connect(
         host="localhost",
         user="root",
@@ -16,13 +14,14 @@ def conectar():
 
 @app.route("/")
 @app.route("/inicio")
-
 def inicio():
-
     return render_template("index.html")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
 
+@app.route("/login", methods=["GET", "POST"])
 def login():
 
     if request.method == "POST":
@@ -31,55 +30,37 @@ def login():
         senha = request.form["senha"]
 
         conexao = conectar()
-
         cursor = conexao.cursor()
 
         sql = """
-        SELECT *
+        SELECT id, usuario
         FROM usuarios
         WHERE usuario = %s AND senha = %s
         """
 
         cursor.execute(sql, (usuario, senha))
-
         resultado = cursor.fetchone()
 
         conexao.close()
 
         if resultado:
-
-            session["usuario"] = resultado[1]
             session["id_usuario"] = resultado[0]
-
+            session["usuario"] = resultado[1]
             return redirect("/aluno")
 
-        else:
-
-            return render_template(
-                "index.html",
-                erro="Usuário ou senha inválidos"
-            )
+        return render_template("login.html", erro="Usuário ou senha inválidos")
 
     return render_template("login.html")
 
-@app.route("/contato")
-
-def contato():
-
-    return render_template("contato.html")
-
 @app.route("/aluno")
-
 def aluno():
 
     if "usuario" not in session:
-
         return redirect("/login")
 
     id_usuario = session["id_usuario"]
 
     conexao = conectar()
-
     cursor = conexao.cursor()
 
     sql = """
@@ -89,24 +70,16 @@ def aluno():
     """
 
     cursor.execute(sql, (id_usuario,))
-
     notas = cursor.fetchall()
 
     conexao.close()
 
-    return render_template(
-        "aluno.html",
-        notas=notas
-    )
+    return render_template("aluno.html", notas=notas)
 
 @app.route("/logout")
-
 def logout():
-
     session.clear()
-
     return redirect("/inicio")
 
 if __name__ == "__main__":
-
     app.run(debug=True)
